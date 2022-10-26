77611c252032cd0cc1c65b8e9c590bb61d9d39a5
Atharva <atharvakutwal2002@gmail.com>
2022-10-26 21:04:53 +0530

    api pages configured

---
+++ components/contact/contact-form.js
@@ -0,0 +1,68 @@
+import { useState } from "react";
+import classes from "./contact-form.module.css";
+
+function ContactForm() {
+  const [enteredEmail, setEnteredEmail] = useState("");
+  const [enteredName, setEnteredName] = useState("");
+  const [enteredMessage, setEnteredMessage] = useState("");
+
+  function sendMessageHandler(event) {
+    event.preventDefault();
+    fetch("/api/contact", {
+      method: "POST",
+      body: JSON.stringify({
+        email: enteredEmail,
+        name: enteredName,
+        message: enteredMessage,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
+
+  return (
+    <section className={classes.contact}>
+      <h1>How can I help you </h1>
+      <form className={classes.form} onSubmit={sendMessageHandler}>
+        <div className={classes.controls}>
+          <div className={classes.control}>
+            <label htmlFor="email">Your Email</label>
+            <input
+              type="email"
+              id="email"
+              required
+              value={enteredEmail}
+              onChange={(e) => setEnteredEmail(e.target.value)}
+            />
+          </div>
+          <div className={classes.control}>
+            <label htmlFor="name">Your Name</label>
+            <input
+              type="text"
+              id="name"
+              required
+              value={enteredName}
+              onChange={(e) => setEnteredName(e.target.value)}
+            />
+          </div>
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="message">Your Message </label>
+          <textarea
+            id="message"
+            rows="5"
+            required
+            value={enteredMessage}
+            onChange={(e) => setEnteredMessage(e.target.value)}
+          ></textarea>
+        </div>
+        <div className={classes.actions}>
+          <button> Send Message </button>
+        </div>
+      </form>
+    </section>
+  );
+}
+
+export default ContactForm;
